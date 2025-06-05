cc638302a2f106588bdab5f5d9f733c4cc14cf9a
A.Mohan <mohan6121@email.com>
2025-06-05 17:22:02 +0530

    mohan

---
+++ src/main/OrderController.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderController {
+
+    @GetMapping("/order")
+    public String home() {
+        return "Welcome to the Order Service";
+    }
+}
