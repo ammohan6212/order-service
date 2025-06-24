e5d825e411e44b9b4f44c20c658b4c0d3093a1d1
A.Mohan <mohan6121@email.com>
2025-06-24 20:23:18 +0530

    mohan

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("")
 public class OrderController {
 
     private final OrderRepository repository;
