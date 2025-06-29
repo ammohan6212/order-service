d0852963a4970c78cb88aad19929cba22fee5120
A.Mohan <mohan6121@email.com>
2025-06-29 15:25:48 +0530

    mohan

--- src/main/java/com/example/repository/OrderRepository.java
+++ src/main/java/com/example/repository/OrderRepository.java
@@ -1,7 +1,8 @@
 package com.example.orders.repository;
-
 import com.example.orders.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -9,6 +10,9 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> findByUsername(String username);
     List<Order> findBySellerName(String sellerName);
     List<Order> findByImageUrl(String imageUrl);
+
+    @Modifying
+    @Transactional
     void deleteByImageUrl(String imageUrl);
 
 }
